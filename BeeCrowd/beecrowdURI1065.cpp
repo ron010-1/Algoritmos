71aa049e1b01e59fbe8b19a3202e97c11b5b2789
Riquelme Oliveira <riquuelmelive@gmail.com>
2023-11-29 13:54:06 -0300

    Organização de Pastas e URI1065

---
+++ BeeCrowd/beecrowdURI1065.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+    int i=6, count=0, num;
+
+    while(i--){
+        scanf("%d", &num);
+        if(abs(num) % 2 == 0){
+            count++; 
+        }
+    }
+
+    printf("%d valores pares\n", count);
+}
